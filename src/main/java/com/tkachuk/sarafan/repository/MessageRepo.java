67af0248fdd373ee6562ad83d061daa3316b8bed
Nazarii Tkachuk <nazar.tkachuk.28@gmail.com>
2020-02-10 12:53:52 +0200

    added MessageController.java, MessageController.java

--- src/main/java/com/tkachuk/sarafan/repository/MessageRepo.java
+++ src/main/java/com/tkachuk/sarafan/repository/MessageRepo.java
@@ -1,4 +1,7 @@
 package com.tkachuk.sarafan.repository;
 
-public interface MessageRepo {
+import com.tkachuk.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepo extends JpaRepository<Message, Long> {
 }
